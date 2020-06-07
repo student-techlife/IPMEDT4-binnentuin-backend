eea173a788f3384dd4e2664e9a6524a4cb6723e2
Lars <larsvanderniet@gmail.com>
2020-06-07 21:27:27 +0200

    Created login screen

---
+++ laravel/resources/views/includes/head.blade.php
@@ -0,0 +1,22 @@
+<title>{{ config('app.name', 'Laravel') }}</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<!--===============================================================================================-->	
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+<!--===============================================================================================-->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+<!--===============================================================================================-->
+
+<script src="{{ asset('js/jquery-3.2.1.min.js') }}" defer></script>
+<script src="{{ asset('js/main.js') }}" defer></script>
