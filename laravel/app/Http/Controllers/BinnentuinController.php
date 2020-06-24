6ad0f9e228136f1aaaf68a734e4a0ceb65c71a1e
draZer0 <ardkokkel@outlook.com>
2020-06-24 14:59:06 +0200

    Comments added in controller

--- laravel/app/Http/Controllers/BinnentuinController.php
+++ laravel/app/Http/Controllers/BinnentuinController.php
@@ -19,19 +19,22 @@ class BinnentuinController extends Controller
       // BINNENTUIN FORM
       if ($request->has('dag_van_week1')) {
 
+        // Check if status is closed. It will change and no need to add times.
         if($request->input('status')=='Gesloten'){
           $binnentuin = Binnentuin::where('dag_van_week', '=', $request->input('dag_van_week1'))->update([
             'status' => $request->input('status')=='Gesloten' ? 0:1
           ]);
           return back()->withStatus(__('De status van de Binnentuin is veranderd naar: Gesloten'));
 
-
+        // If not closed, you need to change the time also
         }else{
 
+          // If there is no time, return an alert as error prevention
           if(!$request->input('openingstijd') or !$request->input('sluitingstijd')){
             return back()->withError(__('Om de Binnentuin weer open te zetten moeten er openingstijden worden ingevoerd.'));
           }
 
+          // Change status and time in database
           $binnentuin = Binnentuin::where('dag_van_week', '=', $request->input('dag_van_week1'))->update([
             'openingstijd' => $request->input('openingstijd'),
             'sluitingstijd' => $request->input('sluitingstijd'),
@@ -45,6 +48,7 @@ class BinnentuinController extends Controller
         // THE ROOF FORM
         else {
 
+          // Check if status is closed. It will change and no need to add times.
           if($request->input('status')=='Gesloten'){
             $theroof = TheRoof::where('dag_van_week', '=', $request->input('dag_van_week'))->update([
               'status' => $request->input('status')=='Gesloten' ? 0:1
@@ -52,12 +56,15 @@ class BinnentuinController extends Controller
 
             return back()->withStatus(__('De status van The Roof is veranderd naar: Gesloten'));
 
+          // If not closed, you need to change the time also
           }else{
 
+            // If there is no time, return an alert as error prevention
             if(!$request->input('openingstijd') or !$request->input('sluitingstijd')){
               return back()->withError(__('Om The Roof weer open te zetten moeten er openingstijden worden ingevoerd.'));
             }
 
+            // Change status and time in database
             $theroof = TheRoof::where('dag_van_week', '=', $request->input('dag_van_week'))->update([
               'openingstijd' => $request->input('openingstijd'),
               'sluitingstijd' => $request->input('sluitingstijd'),
