776b40be93bb83d5dbd85213b966892b7b886a8a
draZer0 <ardkokkel@outlook.com>
2020-06-24 17:14:15 +0200

    Migration changed

--- laravel/database/migrations/2020_06_09_112747_create_binnentuin_table.php
+++ laravel/database/migrations/2020_06_09_112747_create_binnentuin_table.php
@@ -14,9 +14,11 @@ class CreateBinnentuinTable extends Migration
     public function up()
     {
         Schema::create('binnentuin', function (Blueprint $table) {
+          $table->bigIncrements('id');
             $table->string('dag_van_week')->unique();
-            $table->time('openingstijd');
-            $table->time('sluitingstijd');
+            $table->string('openingstijd');
+            $table->string('sluitingstijd');
+            $table->boolean('status')->default(0);
         });
     }
 
